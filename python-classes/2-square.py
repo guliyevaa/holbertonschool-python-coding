2c91256188c62c2bb34a2263d962feb1486a8991
guliyevaa <12545@holbertonstudents.com>
2025-11-21 16:10:28 +0000

    .

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a class Square with a private size attribute and validation."""
+"""This module defines a class Square with a private size attribute and
+validation. It includes a method to compute the area of the square.
+"""
+
+
 class Square:
     """Defines a square with a private size attribute."""
 
     def __init__(self, size=0):
         """Initialize a square with optional size, with type and value checks."""
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError(
+                "size must be an integer"
+            )
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError(
+                "size must be >= 0"
+            )
         self.__size = size
 
     def area(self):
